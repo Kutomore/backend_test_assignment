4bba9ee671f3848a843513482df9239bf3e892a2
Kutomore <kutomore@gmail.com>
2021-07-25 04:03:16 -0300

    Adding serializer unit testing

--- spec/serializers/brand_spec.rb
+++ spec/serializers/brand_spec.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+describe BrandSerializer do
+  let(:brand) {FactoryBot.create(:brand_with_cars)}
+  let(:serialized_brand){ BrandSerializer.new(brand) }
+  let(:expected_json) {
+    {
+      id: brand.id,
+      name: brand.name
+    }.to_json
+  }
 
-RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#to_json" do
+    it "matches the expected output" do
+      expect(serialized_brand.to_json).to eq(expected_json)
+    end
+  end
 end
--- spec/serializers/car_recommendation_spec.rb
+++ spec/serializers/car_recommendation_spec.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+describe CarRecommendationSerializer do
+  let(:car) {
+    FactoryBot.create(:car)
+    Car.all.select('*, NULL as rank_score').first
+  }
+  let(:serialized_car){ CarRecommendationSerializer.new(car) }
+  let(:expected_json) {
+    {
+      id: car.id,
+      brand: {
+        id: car.brand_id,
+        name: car.brand.name
+      },
+      model: car.model,
+      price: car.price,
+      rank_score: 10,
+      label: nil
+    }.to_json
+  }
 
-RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#to_json" do
+    before do
+      car.rank_score = 10
+    end
+
+    it "matches the expected output" do
+      expect(serialized_car.to_json).to eq(expected_json)
+    end
+  end
 end
